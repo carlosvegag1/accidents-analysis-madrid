2056642d0345bed85e92e8f43d9ee44702b17e05
carlosvegag1 <carlosvegagonzalez1@gmail.com>
2025-03-13 11:51:24 +0100

    Optimización del script de análisis y actualización del dataset

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -4,13 +4,13 @@ import pandas as pd
 def cargar_datos():
     """Carga los archivos CSV de la carpeta data/ y los combina en un DataFrame."""
     data_path = "data/"
-    files = ["2019_Accidentalidad.csv", "2020_Accidentalidad.csv", "2021_Accidentalidad.csv",
-             "2022_Accidentalidad.csv", "2023_Accidentalidad.csv", "2024_Accidentalidad.csv"]
+    files = ["2019_accidentalidad.csv", "2020_accidentalidad.csv", "2021_accidentalidad.csv",
+             "2022_accidentalidad.csv", "2023_accidentalidad.csv", "2024_accidentalidad.csv"]
 
     dfs = []
     for file in files:
         file_path = os.path.join(data_path, file)
-        if os.path.exists(file_path):  # Verificar si el archivo existe antes de intentar leerlo
+        if os.path.exists(file_path): 
             dfs.append(pd.read_csv(file_path, delimiter=";"))
         else:
             print(f"Advertencia: {file} no encontrado. Se omitirá.")
